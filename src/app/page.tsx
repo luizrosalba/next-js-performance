1521ed11f0f99ec374d8cfd4f9211e8d54f66bec
luizrosalba <luizrosalba@gmail.com>
2023-07-02 15:58:11 -0300

    won't rerender Addvocado button
    since Addvocado is memoized and props won't change its cached by memoizedAddvocado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Avocado = () => {
   const [count, setCount] = useState(0);
 
-  const addAvocado = () => {
-    setCount(count + 1);
-  };
+  // const addAvocado = () => {
+  //   setCount(count + 1);
+  // };
+
+  const memoizedAddvocado = useCallback(() => {
+    setCount((c) => c + 1);
+  }, [setCount]);
 
   return (
     <React.Fragment>
-      <Addvocado add={addAvocado} />
+      <Addvocado add={memoizedAddvocado} />
       <div>{Array(count).fill('🥑').join(',')}</div>
     </React.Fragment>
   );
 };
 
-const Addvocado = ({ add }: { add: () => void }) => {
+const Addvocado = React.memo(({ add }: { add: () => void }) => {
   console.log('component re-rendered');
 
   return <button onClick={add}>Add avocado</button>;
-};
+});
+
+Addvocado.displayName = 'Addvocado';
 
 export default Avocado
