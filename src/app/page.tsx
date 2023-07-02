115f0e060d1a939065fc97d7bfc5585d65ff5059
luizrosalba <luizrosalba@gmail.com>
2023-07-02 18:43:47 -0300

    two examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <React.Fragment>
+      <p>This page aims to explain why components Rerender</p>
+      <p><Link href={'./stateChanges'}>1) State Changes </Link></p>
+      <p><Link href={'./parentChanges'}>2) Parent Changes </Link></p>
+      <p><Link href={'./stateChanges'}>3) Context Changes </Link></p>
+      <p><Link href={'./stateChanges'}>4) Hooks Changes </Link></p>
+      <p><Link href={'./stateChanges'}>5) Not memoized components will always rerenders when props changes </Link></p>
+
       <Link href={'./memo'}>Memoization using useCallback and useMemo. Examples From <Link href={'https://webtips.dev/webtips/react-hooks/usecallback?_branch_match_id=1119042556059019777&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9bLTU3JLM3VS87P1U%2F3yzItMHYLN3NMAgCpG4g8IwAAAA%3D%3D'}>this page</Link> </Link>
     </React.Fragment>
   );
---
+++ src/app/parentChanges/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { useCallback, useState } from 'react';
+
+const Child = () => {
+    console.log('Child rerender')
+    return (
+        <div>{'====>'}Child</div>
+    )
+}
+
+const Parent = () => {
+    const [count, setCount] = useState(0);
+    console.log('Parent rerender')
+    return (
+        <>
+            {/* will rerender when Parent rerenders */}
+            <div>Parent</div>
+            <Child />
+            <button onClick={() => setCount(count + 1)}>Click to Add</button>
+        </>
+
+    );
+};
+
+export const ParentChanges = () => {
+
+    console.log('rerenders')
+
+    return (
+        <React.Fragment>
+            <p>
+                A component will re-render itself if its parent re-renders. Or, if we look at this from the opposite direction: when a component re-renders, it also re-renders all its children.
+            </p>
+            <p>
+                It always goes “down” the tree: the re-render of a child doesn’t trigger the re-render of a parent. (There are a few caveats and edge cases here, see the full guide for more details: The mystery of React Element, children, parents and re-renders).
+            </p>
+            <p>Click on the button and look at console.log</p>
+
+            <Parent />
+
+        </React.Fragment>
+    );
+};
+
+
+
+export default ParentChanges
