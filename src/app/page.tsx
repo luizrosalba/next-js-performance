f492313accb3461fdb90578bd9343ad360f97027
luizrosalba <luizrosalba@gmail.com>
2023-07-02 18:24:35 -0300

    Fix routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <React.Fragment>
-      <Link href={'/memo'}>Memoization using useCallback and useMemo </Link>
+      <Link href={'./memo'}>Memoization using useCallback and useMemo. Examples From <Link href={'https://webtips.dev/webtips/react-hooks/usecallback?_branch_match_id=1119042556059019777&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9bLTU3JLM3VS87P1U%2F3yzItMHYLN3NMAgCpG4g8IwAAAA%3D%3D'}>this page</Link> </Link>
     </React.Fragment>
   );
 };
