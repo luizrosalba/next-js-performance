75416ec98f7ac1906f3b0b8a90e7b454f0030952
luizrosalba <luizrosalba@gmail.com>
2023-07-10 10:37:00 -0300

    Fix example

--- src/app/contextChanges/page.tsx
+++ src/app/contextChanges/page.tsx
@@ -2,17 +2,18 @@
 import Link from 'next/link';
 import React, { createContext, useCallback, useContext, useState } from 'react';
 
-const ExampleContext = createContext(0);
+const ExampleContext = createContext([]);
 
 const Parent = () => {
-    const useValue = useContext(ExampleContext) /// when value changes
-    const [count, setCount] = useState(0);
-    console.log('Parent rerender')
+    const [count, setCount] = useContext(ExampleContext); /// when value changes
+    console.log('Component rerender')
     return (
         <>
             {/* will rerender when Parent rerenders */}
-            <div>Parent</div>
-            <button onClick={() => setCount(count + 1)}>Click to Add</button>
+            <div>Count: {count}</div>
+            <button onClick={() => setCount(count + 1)}>
+                Change Context Value
+            </button>
         </>
 
     );
@@ -21,14 +22,14 @@ const Parent = () => {
 const ContextChanges = (): JSX.Element => {
 
     console.log('rerenders')
-    const level = useContext(ExampleContext);
+    const [count, setCount] = useState(0);
     return (
         <React.Fragment>
             <p>
                 When the value in Context Provider changes, all components that use this Context will re-render, even if they don’t use the changed portion of the data directly. Those re-renders can not be prevented with memoization directly, but there are a few workarounds that can simulate it (see preventing re-renders caused by Context)
             </p>
             <p>Click on the button and look at console.log</p>
-            <ExampleContext.Provider value={level + 1}>
+            <ExampleContext.Provider value={[count, setCount]}>
                 <Parent />
             </ExampleContext.Provider>
             <div>
